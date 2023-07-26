3270c30f5f695bd1cb49e317c7176e970c68223d
= <ardayildirim05@icloud.com>
2023-07-26 12:38:27 +0300

    Implement zod validation schema for user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,51 @@
 import typegoose, { modelOptions, prop, Severity } from "@typegoose/typegoose";
+import z from "zod";
+
+const usernameMinLength = 1;
+const usernameMaxLength = 50;
+const emailMinLength = 5;
+const emailMaxLength = 100;
+const passwordMinLength = 8;
+const passwordMaxLength = 50;
+const passwordHashMaxLength = 1000;
+
+const minLengthMessage = (property: string, min: number) =>
+  `${property} has to have ${min} or more characters`;
+const maxLengthMessage = (property: string, max: number) =>
+  `${property} has to have ${max} or less characters`;
+
+const schema = z
+  .object({
+    username: z
+      .string()
+      .min(passwordMinLength, minLengthMessage("Username", usernameMinLength))
+      .max(passwordMaxLength, maxLengthMessage("Username", usernameMaxLength)),
+
+    email: z
+      .string()
+      .email("Input has to be an email")
+      .min(emailMinLength, minLengthMessage("Email", emailMinLength))
+      .max(emailMaxLength, maxLengthMessage("Email", emailMaxLength)),
+
+    password: z
+      .string()
+      .min(passwordMinLength, minLengthMessage("Password", passwordMinLength))
+      .max(passwordMaxLength, maxLengthMessage("Password", passwordMaxLength)),
+
+    confirmPassword: z
+      .string()
+      .min(
+        passwordMinLength,
+        minLengthMessage("Confirm password", passwordMinLength)
+      )
+      .max(
+        passwordMaxLength,
+        maxLengthMessage("Confirm Password", passwordMaxLength)
+      ),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Confirm password must match password",
+  });
 
 @modelOptions({
   schemaOptions: {

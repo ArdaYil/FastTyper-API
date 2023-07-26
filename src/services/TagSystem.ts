6aef1090994b81ef00bbe1f2f201e506cab10c76
= <ardayildirim05@icloud.com>
2023-07-26 23:14:52 +0300

    Implement tag system

---
+++ src/services/TagSystem.ts
@@ -0,0 +1,73 @@
+import Mathematics from "./Mathematics";
+import TagModel from "../models/tag";
+
+const charactersPerTag = 7;
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const randomCharacter = () => {
+  const randomNumber = Mathematics.random(1, characters.length - 1);
+
+  return characters[randomNumber];
+};
+
+const generateTag = () => {
+  let tag = "";
+
+  for (let i = 0; i < charactersPerTag; i++) {
+    tag += randomCharacter();
+  }
+
+  return tag;
+};
+
+const generateUniqueTag = async () => {
+  const attempts = 10;
+
+  for (let i = 0; i < attempts; i++) {
+    const tag = generateTag();
+
+    if (await isTagUnique(tag)) {
+      const tagDocument = new TagModel({ value: tag });
+
+      await tagDocument.save();
+
+      return tag;
+    }
+  }
+
+  return "";
+};
+
+const isTagUnique = async (tag: string) => {
+  return TagModel.findOne({ value: tag });
+};
+
+export default generateUniqueTag;

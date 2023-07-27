03e0c0bbd38c0059d0a75bc25f813074e4d15129
= <ardayildirim05@icloud.com>
2023-07-27 20:25:09 +0300

    Finish the implementation of the post endpoint of the user router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,19 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import validate from "../middleware/validate";
-import { User, validateUser } from "../models/user";
+import UserModel, { User, validateUser } from "../models/user";
 
 const router = Router();
 
-router.post("/", [validate<User>(validateUser)], (req, res) => {
-  res.status(200).send("Success");
-});
+router.post(
+  "/",
+  [validate<User>(validateUser)],
+  async (req: Request, res: Response) => {
+    const { body } = req;
+    const user = new UserModel(body);
+    const accessToken = user.generateAccessToken();
+
+    await user.save();
+
+    res.status(200).json(user).setHeader("x-auth-token", accessToken);
+  }
+);

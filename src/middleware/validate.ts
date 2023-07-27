d81a7d0a63f6cdd172e4b2acb13f107df0ea36a0
= <ardayildirim05@icloud.com>
2023-07-27 20:17:14 +0300

    Create validation middleware

---
+++ src/middleware/validate.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { SafeParseReturnType } from "zod";
+
+const validate =
+  <T>(validator: Function) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result: SafeParseReturnType<T, {}> = validator();
+
+    if (!result.success) return res.status(404).send(result.error.message);
+
+    next();
+  };

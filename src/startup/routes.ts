1b29be5dff92e81663b15c18e5e268ac86de06a7
= <ardayildirim05@icloud.com>
2023-08-01 18:16:16 +0300

    Integrate tag route into project

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import words from "../routes/words";
+import tag from "../routes/tag";
 
 const routes = (app: Application) => {
   app.use("/api/words", words);
+  app.use("/api/tag", tag);
 };
 
 export default routes;

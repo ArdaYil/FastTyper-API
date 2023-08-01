9fdcf0cec452ffe2614acd81950b3267b95120f5
= <ardayildirim05@icloud.com>
2023-08-01 22:37:13 +0300

    Secure api/tag/:id with authentication and authorization middleware

--- src/routes/tag.ts
+++ src/routes/tag.ts
@@ -1,15 +1,23 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import TagModel from "../models/tag";
+import auth from "../middleware/auth";
+import admin from "../middleware/admin";
 
 const router = Router();
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const tag = TagModel.findById(id);
+const getIdPermissionLevel = 10;
 
-  if (!tag) return res.status(404).send("Tag not found");
+router.get(
+  "/:id",
+  [auth, admin(getIdPermissionLevel, true)],
+  (req: Request, res: Response) => {
+    const id = req.params.id;
+    const tag = TagModel.findById(id);
 
-  res.status(200).json(tag);
-});
+    if (!tag) return res.status(404).send("Tag not found");
+
+    res.status(200).json(tag);
+  }
+);
 
 export default router;

01b86e1a645d2e78e11d939a0276217c80b35898
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-04 17:31:34 +0300

    Properly handle errors in tag get endpoint, and make endpoint handler async

--- src/routes/tag.ts
+++ src/routes/tag.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import TagModel from "../models/tag";
 import auth from "../middleware/auth";
 import admin from "../middleware/admin";
+import { CastError } from "mongoose";
 
 const router = Router();
 
@@ -10,13 +11,21 @@ const getIdPermissionLevel = 10;
 router.get(
   "/:id",
   [auth, admin(getIdPermissionLevel)],
-  (req: Request, res: Response) => {
-    const id = req.params.id;
-    const tag = TagModel.findById(id);
+  async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id;
+      const tag = await TagModel.findById(id);
 
-    if (!tag) return res.status(404).send("Tag not found");
+      if (!tag) return res.status(404).send("Tag not found");
 
-    res.status(200).json(tag);
+      return res.status(200).json(tag);
+    } catch (exception: any) {
+      if (exception.kind === "ObjectId") {
+        return res.status(404).send("Tag not found");
+      }
+
+      return res.status(500).send("Internal server error");
+    }
   }
 );
 

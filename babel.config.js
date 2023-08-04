384dad1e273b1fac6f9f10ca26c8e91ec4c7f372
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-04 17:05:03 +0300

    Update babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports={
     "presets": ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-    "plugins": []
+    "plugins": [
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
+        "@babel/plugin-transform-class-properties",
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+    ["@babel/plugin-transform-private-methods", { "loose": true }]
+      ]
     }

55da7d4db0ec2a1a1b976ea83045bd1df6167e51
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-04 17:29:33 +0300

    Add typegoose model options to tag model

--- src/models/tag.ts
+++ src/models/tag.ts
@@ -1,10 +1,23 @@
-import typegoose, { prop } from "@typegoose/typegoose";
+import typegoose, {
+  prop,
+  modelOptions,
+  Severity,
+  getModelForClass,
+} from "@typegoose/typegoose";
 
-class Tag {
-  @prop({ required: true, minlength: 7, maxlength: 15, Type: String })
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+  options: {
+    allowMixed: Severity.ALLOW,
+  },
+})
+export class Tag {
+  @prop({ required: true, minlength: 7, maxlength: 15, type: String })
   value: string;
 }
 
-const TagModel = typegoose.getModelForClass(Tag);
+const TagModel = getModelForClass(Tag);
 
 export default TagModel;

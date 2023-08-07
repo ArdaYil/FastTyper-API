3fa04d0ea993d3ec6ca1ef9dacfbbc51fa5a1c48
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-07 21:51:33 +0300

    Make so that values can be fed in into request mocks

--- test/util/requestMock.ts
+++ test/util/requestMock.ts
@@ -1,8 +1,20 @@
+import { User } from "../../src/models/user";
+
 class RequestMock {
   body: any;
   params: any;
+  user?: User;
+
+  constructor(obj?: object, config: object = {}) {
+    this.body = obj;
+
+    for (let property in config) {
+      this[property] = config[property];
+    }
+  }
 }
 
-const getRequest = (obj?: object) => new RequestMock();
+const getRequest = (obj?: object, config?: object) =>
+  new RequestMock(obj, config);
 
 export default getRequest;

5945bd9c67894d4e74e14d02e4901321b8983ff8
Arda Yildirim <ardayildirim05@icloud.com>
2023-08-07 08:27:30 +0300

    Implement mock for request

---
+++ test/util/middlewareMocks.ts
@@ -0,0 +1,36 @@
+class RequestClass {
+  public currentStatus?: number;
+  public currentMessage: string;
+  public data: Array<any> | object;
+
+  private isSent() {
+    return this.currentMessage != null || this.data != null;
+  }
+
+  status(status: number) {
+    if (this.currentStatus != null)
+      throw new Error("Request already have status");
+
+    this.currentStatus = status;
+
+    return this;
+  }
+
+  send(message: string) {
+    if (this.isSent()) throw new Error("Request already sent");
+
+    this.currentMessage = message;
+
+    return this;
+  }
+
+  json(data: Array<any> | object) {
+    if (this.isSent()) throw new Error("Request already sent data");
+
+    this.data = data;
+
+    return this;
+  }
+}
+
+const getRequest = (obj: object) => new RequestClass();
